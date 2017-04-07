fa95a868cc2d02b5c0dd60405edbd0c215000369
Endra Wahyu Wardana_6706154062 <e.wardana182@gmail.com>
2017-04-07 10:35:11 +0700

    Update gambar.java

--- gambar.java
+++ gambar.java
@@ -9,20 +9,20 @@ class gambar {
 
     for (int i = 1; i < n; i += 2) {
       for (int j = 0; j < 4 - i / 2; j++)
-        System.out.print(" ");
+        System.out.print("-");
 
       for (int j = 0; j < i; j++)
-        System.out.print("*");
-        System.out.print("\n");
+        System.out.print("o");
+        System.out.print("-\n");
     }
 
     for (int i = n; i > 0; i -= 2) {
       for (int j = 0; j < 4 - i / 2; j++)
-        System.out.print(" ");
+        System.out.print("-");
 
       for (int j = 0; j < i; j++)
-        System.out.print("*");
-        System.out.print("\n");
+        System.out.print("o");
+        System.out.print("-\n");
     }
   }
 }
